3d7b932c4d0c5be7732893e440e19cbfe4702d5c
Abdur Rahman Saad <abdurrahmansaad46@gmail.com>
2025-09-17 16:39:51 +0600

    create a document app

---
+++ src/documents/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Document(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(default='Title')
+    content = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.active and self.active_at is None:
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
